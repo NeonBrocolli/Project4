d3347072e8b17c3d0c4e420782d259aa8fb9d9e5
Chris De Vera <chrisdv25@hotmail.com>
2017-06-05 15:27:37 -0700

    added pages folder, setting up users

--- gg_app/src/pages/App/App.js
+++ gg_app/src/pages/App/App.js
@@ -3,16 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+/*---lifecycle methods---*/
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <div className="header">
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
